207ec274605b80badca832489cf7cec25d58c4f9
gitlet <11thdim@gmail.com>
2018-01-16 19:13:41 +0530

    uploading the app filesm first go

---
+++ assets/scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function alertMsg() {
+  $('body #alert').on('click', function() {
+    $('body #hidden-msg').css('display', 'block');
+  });
+});
+
+// $(document).ready(function alertMsg() {
+//   // alert('hellooooooo');
+//   // var x, y, res;
+//   var x = prompt('Enter x:');
+//   var y = prompt('Enter y:');
+//   var res = parseInt(x) + parseInt(y);
+//   alert("Answer: " + res);
+// });
+
+// alertMsg();
+
+// No! Please don't consider this to be a waste of time, rather it's a fun way to polish or revisit my skills in webdev. Sincerely hope oyu all'll enjoy like I do.. thanks! - Your's Abdullah
